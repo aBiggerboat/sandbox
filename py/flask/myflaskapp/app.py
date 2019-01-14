4564d244294f2694e3c469c7db7668b32e6e6548
Clint <aBiggerboat@users.noreply.github.com>
2019-01-14 07:30:27 -0600

    Update app.py

--- py/flask/myflaskapp/app.py
+++ py/flask/myflaskapp/app.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1977'
-app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Init MySQL
